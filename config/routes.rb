656de4dd0720092f8ca029e97a9ee86cb6f30963
Gilbert <mont-phil@hotmail.fr>
2014-07-28 01:00:58 -0400

    error page setup no server

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/home'
   root "pages#home"
+  if Rails.env.production?
+    get '404', :to => 'pages#page_not_found'
+    get '422', :to => 'pages#server_error'
+    get '500', :to => 'pages#server_error'
+  end
 end
